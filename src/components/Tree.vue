98d0107d1e6993f7e4d92aae501c00ea8c0a118d
Xiangpeng Hao <xha62@sfu.ca>
2019-08-24 16:32:29 -0700

    update index on fly

--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -22,12 +22,6 @@ export default {
     };
   },
   mounted() {
-    let newTreeData = large_insert[49].t;
-    // Set the dimensions and margins of the diagram
-    let margin = { top: 20, right: 90, bottom: 30, left: 90 },
-      width = 960 - margin.left - margin.right,
-      height = 1000 - margin.top - margin.bottom;
-
     // append the svg object to the body of the page
     // appends a 'group' element to 'svg'
     // moves the 'group' element to the top left margin
